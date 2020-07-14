a06e36361a64665b924de6592ec3a06a188a9f4f
Chris Purbaugh <cpurbaugh@sea-fire.com>
2020-07-14 18:34:22 +0000

    Adding contract delivery

---
+++ metalcraft/metalcraft/doctype/contract_delivery/contract_delivery.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Metalcraft, Inc. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Contract Delivery', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ metalcraft/metalcraft/doctype/contract_delivery/test_contract_delivery.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Metalcraft, Inc. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestContractDelivery(unittest.TestCase):
+	pass
